6957b39e6ba642f4ff79a564444d15994d4746e2
Igor Ying Zhang <igor_zhangying@fudan.edu.cn>
2023-03-28 18:17:06 +0800

    IYZ: add some comment to the code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,43 @@
 //! # Rust-based Electronic-Structure Tool (REST)
 //! 
+//! ### Installation
+//!   At present, REST can be compiled in Linux only  
+//! 
+//!   0) Prerequisites:  
+//!     - libopenblas.so  
+//!     - libcint.so  
+//!     - libhdf5.so  
+//!     - libxc.so  
+//!   1) git clone git@github.com:igor-1982/rest_workspace.git rest_workspace  
+//!   2) cd rest_workspace; cp Config.templet Config
+//!   3) edit `Config` to make the prerequeisite libraries aformationed accessable via some global variables heading with `REST`.  
+//!   4) bash Config; source $HOME/.bash_profile
+//!   5-1) cargo build (--release) 
+//! 
+//! ### Usage
+//!   Some examples are provided in the folder of `rest/examples/`.  
+//!   Detailed user manual is in preparation.
+//! 
+//! ### Features
+//!   1) Use Gaussian Type Orbital (GTO) basis sets  
+//!   2) Provide Density Functional Approximations (DFAs) at varying levels from LDA, GGA, Hybrid, to Fifth-rungs, including doubly hybrid approximations, like XYG3, XYGJ-OS, and random-phase approximation (RPA).  
+//!   3) Provide some Wave Function Methods (WFMs), like Hartree-Fock approximation (HF) and Moller-Plesset Second-order perturbation (MP2)
+//!   4) Provide analytic electronic-repulsive integrals (ERI)s as well as the Resolution-of-idensity (RI) approximation. The RI algorithm is the recommended choice.  
+//!   5) High Share Memory Parallelism (SMP) efficiency
+//! 
+//! 
+//! ### Development
+//!   1) Provide a tensor library, namely [`rest_tensors`](https://igor-1982.github.io/rest_tensors/rest_tensors/). `rest_tensors` is developed to manipulate
+//!    different kinds multi-rank arrays in REST. Thanks to the sophisticated generic, type, and trait systems, `rest_tensors` can be used as easy as `Numpy` and `Scipy` without losing the computation efficiency. 
+//!   2) It is very easy and save to develop a parallel program using the Rust language. Please refer to [`rayon`](rayon) for more details.
+//!   3) However, attention should pay if you want to use (Sca)Lapcke functions together in the rayon-spawn threads. 
+//!    It is because the (Sca)Lapack functions, like `dgemm`, were compiled with OpenMP by default.  The competetion between OpenMP and Rayon threads 
+//!    could dramatically deteriorate the final performance.  
+//!    Please use [`utilities::omp_set_num_threads_wrapper`] to set the OpenMP treads number in the runtime.
+//! 
+//! 
+//! 
+//! ### Presentation
 //! ![image](/home/igor/Documents/Package-Pool/rest_workspace/rest/figures/REST电子结构程序1.png) 
 //! ![image](/home/igor/Documents/Package-Pool/rest_workspace/rest/figures/REST电子结构程序2.png) 
 //! ![image](/home/igor/Documents/Package-Pool/rest_workspace/rest/figures/REST电子结构程序3.png) 
